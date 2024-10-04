5a3eef437c9e39558292bb0dce2df35bbdef892b
Tacha <Anastasiykoshe@gmail.com>
2024-10-04 15:45:26 +0300

    Модуль 10 задание 3

---
+++ hw3.py
@@ -0,0 +1,45 @@
+from threading import Thread, Lock
+import random
+import time
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = Lock()
+
+    def deposit(self):
+        for i in range(100):
+            amount = random.randint(50, 500)
+            with self.lock:
+                self.balance += amount
+                if self.balance >= 500 and self.lock.locked():
+                    self.lock.release()
+                print(f'Пополнение: {amount}, Баланс: {self.balance}')
+            time.sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            amount = random.randint(50, 500)
+            print(f'Запрос на {amount}')
+            with self.lock:
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f'Снятие: {amount}, баланс: {self.balance}')
+                else:
+                    print('Запрос отклонён, недостаточно средств')
+                    self.lock.acquire()
+            time.sleep(0.001)
+
+
+bk = Bank()
+
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
