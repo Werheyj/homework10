e2c8e5ce737f9fa3985b769c2f4843e29f209573
Tacha <Anastasiykoshe@gmail.com>
2024-10-04 16:25:19 +0300

    Модуль 10 задание 3 Без оператора with

--- hw3.py
+++ hw3.py
@@ -11,35 +11,41 @@ class Bank:
     def deposit(self):
         for i in range(100):
             amount = random.randint(50, 500)
-            with self.lock:
+            self.lock.acquire()
+            try:
                 self.balance += amount
                 if self.balance >= 500 and self.lock.locked():
-                    self.lock.release()
-                print(f'Пополнение: {amount}, Баланс: {self.balance}')
+                    print(f'Пополнение: {amount}, Баланс: {self.balance}')
+            finally:
+                self.lock.release()
             time.sleep(0.001)
 
     def take(self):
         for i in range(100):
             amount = random.randint(50, 500)
             print(f'Запрос на {amount}')
-            with self.lock:
+            self.lock.acquire()
+            try:
                 if amount <= self.balance:
                     self.balance -= amount
                     print(f'Снятие: {amount}, баланс: {self.balance}')
                 else:
                     print('Запрос отклонён, недостаточно средств')
-                    self.lock.acquire()
+            finally:
+                self.lock.release()
             time.sleep(0.001)
 
 
 bk = Bank()
 
-th1 = Thread(target=Bank.deposit, args=(bk,))
-th2 = Thread(target=Bank.take, args=(bk,))
+th1 = Thread(target=bk.deposit)
+th2 = Thread(target=bk.take)
 
 th1.start()
 th2.start()
+
 th1.join()
 th2.join()
 
+print()
 print(f'Итоговый баланс: {bk.balance}')
